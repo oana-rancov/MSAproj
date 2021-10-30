6cb0b27c6751ea18adcc041e68d5f8ae594d44d4
oana-rancov <oanamariarancov@yahoo.com>
2021-10-30 11:01:07 +0300

    Register fields

---
+++ app/src/main/java/com/example/msaproj/RegisterActivity.java
@@ -0,0 +1,34 @@
+package com.example.msaproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private EditText firstName, lastName, userName, email, password;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        firstName = findViewById(R.id.registerFirstName);
+        lastName = findViewById(R.id.registerLastName);
+        userName = findViewById(R.id.registerUsername);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        register = findViewById(R.id.btRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
