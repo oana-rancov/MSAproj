e25487ced0538fe8dee8c2c166ab37235d4f529c
andreea lenghel <deabeacodobea@yahoo.com>
2021-11-08 08:40:02 +0200

    register updates

--- app/src/main/java/com/example/msaproj/Users.java
+++ app/src/main/java/com/example/msaproj/Users.java
@@ -2,60 +2,20 @@ package com.example.msaproj;
 
 public class Users {
     //private String id;
-    private String first_name;
-    private String last_name;
-    private String username;
-    private String hash_pass;
-    private String email;
+    public String first_name;
+    public String last_name;
+    public String username;
+    public String hash_pass;
+    public String email;
 
     public Users() {
     }
-
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
+    public Users(String first_name, String last_name, String username, String hash_pass, String email ) {
         this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
         this.last_name = last_name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getHash_pass() {
-        return hash_pass;
-    }
-
-    public void setHash_pass(String hash_pass) {
         this.hash_pass = hash_pass;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
+
 }
