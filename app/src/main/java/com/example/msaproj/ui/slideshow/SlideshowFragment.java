d2f6f2c56706c091dae6bf01e395232ccfb7e5c6
“oana-rancov” <oanamariarancov@yahoo.com>
2021-11-22 23:53:18 +0200

    Share fragnment done

--- app/src/main/java/com/example/msaproj/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/msaproj/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,11 @@
 package com.example.msaproj.ui.slideshow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,14 +30,30 @@ public class SlideshowFragment extends Fragment {
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
+        final TextView textView = binding.textShare;
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        Button buttonShare = (Button)root.findViewById(R.id.btShare);
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                String shareBody = "Your body here";
+                String shareSub = "Your subject here";
+                intent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(intent, "Share using"));
+            }
+        });
+
         return root;
+
     }
 
     @Override
