2a4f964bbc4434101fc9aee015b7279184c02bd5
“oana-rancov” <oanamariarancov@yahoo.com>
2021-12-29 20:12:39 +0200

    Added spinner dropdown

--- app/src/main/java/com/example/msaproj/AddActivity.java
+++ app/src/main/java/com/example/msaproj/AddActivity.java
@@ -1,64 +1,68 @@
 package com.example.msaproj;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+  private static final String TAG = AddActivity.class.getSimpleName();
   private TextView title;
   Button saveBt;
-  private EditText add_sum, add_category, add_type, add_month;
+  private EditText add_sum, add_category, add_type;
+  Spinner spinnerMonths;
+
+  String textMonthSelected;
+
   FirebaseDatabase firebaseDatabase;
   DatabaseReference databaseReference;
   IncomesExpenses incomesexpenses;
 
   @Override
     public void onCreate(Bundle savedInstanceState) {
+    Log.d(TAG, "Created!!!!!");
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_add);
     add_type = (EditText) findViewById(R.id.selection_type);
 
     add_category = (EditText) findViewById(R.id.add_category);
-    add_month = (EditText) findViewById(R.id.month);
 
     title = (TextView) findViewById(R.id.title);
     add_sum = (EditText) findViewById(R.id.add_sum);
     saveBt = (Button) findViewById(R.id.saveBt);
+    spinnerMonths = (Spinner) findViewById(R.id.spinnerMonths);
 
     firebaseDatabase = FirebaseDatabase.getInstance();
     databaseReference = firebaseDatabase.getReference("Incomes/Expenses");
     incomesexpenses = new IncomesExpenses();
+
+    // Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.months_array, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+    spinnerMonths.setAdapter(adapter);
+
+    spinnerMonths.setOnItemSelectedListener(this);
 
     saveBt.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -66,7 +70,6 @@ public class AddActivity extends AppCompatActivity {
         String sum= add_sum.getText().toString().trim();
         String category=add_category.getText().toString().trim();
         String type = add_type.getText().toString().trim();
-        String month =add_month.getText().toString().trim();
 
       //insert drop down list
 
@@ -88,18 +91,14 @@ public class AddActivity extends AppCompatActivity {
 
         }
 
-        if(month.isEmpty()) {
-          add_month.setError("Please provide the month");
-          add_month.requestFocus();
 
-        }
 
         //rootNode = FirebaseDatabase.getInstance();
        // reference = rootNode.getReference("months");
         //reference = rootNode.getReference(String.valueOf(spinnerMonth));
         //reference.setValue("first entry");
         String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        addDatatoFirebase(currentuser, month, sum, category, type);
+        addDatatoFirebase(currentuser, textMonthSelected, sum, category, type);
       }
 
 
@@ -127,5 +126,15 @@ public class AddActivity extends AppCompatActivity {
       }
     });
   }
+
+  @Override
+  public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    String textMonthSelected = adapterView.getItemAtPosition(i).toString();
   }
+
+  @Override
+  public void onNothingSelected(AdapterView<?> adapterView) {
+
+  }
+}
 
