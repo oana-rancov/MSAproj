4101dccbd9489c45f7214212f14de8755ee941cd
“oana-rancov” <oanamariarancov@yahoo.com>
2021-12-31 17:07:06 +0200

    Created logout button

--- app/src/main/java/com/example/msaproj/ui/logout/LogoutFragment.java
+++ app/src/main/java/com/example/msaproj/ui/logout/LogoutFragment.java
@@ -1,9 +1,11 @@
 package com.example.msaproj.ui.logout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.msaproj.AddActivity;
+import com.example.msaproj.LogInActivity;
+import com.example.msaproj.NavigationActivity;
+import com.example.msaproj.R;
 import com.example.msaproj.databinding.FragmentGalleryBinding;
 import com.example.msaproj.databinding.FragmentLogoutBinding;
 import com.example.msaproj.ui.gallery.GalleryViewModel;
@@ -19,6 +25,7 @@ import com.example.msaproj.ui.gallery.GalleryViewModel;
 public class LogoutFragment extends Fragment {
     private LogoutViewModel logoutViewModel;
     private FragmentLogoutBinding binding;
+    private Button buttonLogout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +35,16 @@ public class LogoutFragment extends Fragment {
         binding = FragmentLogoutBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        buttonLogout = (Button) root.findViewById(R.id.btn_logout);
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+
         final TextView textView = binding.textLogout;
         logoutViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -36,6 +53,7 @@ public class LogoutFragment extends Fragment {
             }
         });
         return root;
+
     }
 
     @Override
